86143a8e20bf586d5d94a9c341d88bfe994a0f62
Agustin Vera <ronedev25@gmail.com>
2023-11-08 23:48:12 -0300

    fix: config to deploy

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ const router = express.Router()
 //inicializamos el routes con express
 
 //Obtener todos los productos
-router.get('/', productController.getAllProducts)
+router.get('/', (req, res) => res.json("API CODIFICADORES"))
+
+//Obtener todos los productos
 router.get('/products', productController.getAllProducts)
 
 //Obtener un solo producto especifico
